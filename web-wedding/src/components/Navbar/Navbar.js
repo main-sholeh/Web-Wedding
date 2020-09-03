12309ab8bbf7d4baa77189b3c39b4cea1ce87c5c
main-sholeh <59788014+main-sholeh@users.noreply.github.com>
2020-09-03 11:35:11 +0700

    Hero Section
    Membuat Hero Section

---
+++ web-wedding/src/components/Navbar/Navbar.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import { MdFingerprint } from "react-icons/md";
+import { FaRegHeart } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Button } from "../Button/Button";
+import "../Navbar/Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <FaRegHeart className="navbar-icon" />
+            WEDDING
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Groom & Bride
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Love Story
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Gallery
+              </Link>
+            </li>
+            {/* <li className="nav-btn">
+              {button ? (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline">SIGN UP</Button>
+                </Link>
+              ) : (
+                <Link to="/sign-up" className="btn-link">
+                  <Button buttonStyle="btn--outline" buttonSize="btn--mobile">
+                    Gallery
+                  </Button>
+                </Link>
+              )}
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
